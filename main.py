36433430e52535e185c5ae83c56e03cea2de6197
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-09-08 22:38:51 -0500

    Camera

--- main.py
+++ main.py
@@ -21,18 +21,21 @@ class Game:
                 if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                     running = False
 
-            if Frame != None:
-                Frame.frameUpdate()
-                for entity in Frame.Entities:
-                    entity.frameUpdate()
-                    entity.draw(self.screen)
+            if self.Frame != None:
+                self.Frame.frameUpdate()
 
             pygame.display.flip()
 
         pygame.quit()
 
+class Camera:
+    Position = pygame.Vector2(0, 0)
+    def __init__(self):
+        pass
+        
 class Frame:
     Entities = []
+    Camera = Camera()
     
     def __init__(self):
         pass
@@ -43,14 +46,27 @@ class Frame:
     def destroyEntity(self, entity):
         self.Entities.remove(entity)
         
-    def frameUpdate():
-        pass
+    def frameUpdate(self):
+        for entity in Frame.Entities:
+            entity.frameUpdate()
+            entity.draw(game.screen)
 
 class GameFrame(Frame):
     def __init__(self):
+        super().__init__()
         self.createEntity(Traveler())
         pass
         
+    def frameUpdate(self):
+        super().frameUpdate()
+        if pygame.key.get_pressed()[pygame.K_w]:
+            self.Camera.Position.y -= 1
+        if pygame.key.get_pressed()[pygame.K_s]:
+            self.Camera.Position.y += 1
+        if pygame.key.get_pressed()[pygame.K_a]:
+            self.Camera.Position.x -= 1
+        if pygame.key.get_pressed()[pygame.K_d]:
+            self.Camera.Position.x += 1
 
 class Entity:
     Position = pygame.Vector2(0, 0)
@@ -83,7 +99,7 @@ class Entity:
 
     def draw(self, screen):
         if self.Visible:
-            screen.blit(self.sprite.image, self.Position)
+            screen.blit(self.sprite.image, self.Position - game.Frame.Camera.Position)
         
     def frameUpdate(self):
         pass
@@ -96,21 +112,7 @@ class Traveler(Entity):
         super().__init__()
         
     def frameUpdate(self):
-        #test movement
-        
-        if pygame.key.get_pressed()[pygame.K_s]:
-            self.Position.y += 1
-            self.Direction = 2
-        if pygame.key.get_pressed()[pygame.K_a]:
-            self.Position.x -= 1
-            self.Direction = 3
-            
-        if pygame.key.get_pressed()[pygame.K_w]:
-            self.Position.y -= 1
-            self.Direction = 0
-        if pygame.key.get_pressed()[pygame.K_d]:
-            self.Position.x += 1
-            self.Direction = 1
+        super().frameUpdate()
             
         directionImage = f"assets/sprites/traveler_{self.Direction}.png"
         if self.texture != directionImage:
